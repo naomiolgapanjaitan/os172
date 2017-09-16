594ff52360d2e85c1fb2fb90ab0b5e6ae244229f
NAOMI OLGA PANJAITAN - 1706106873 <naomi.olga@badak.cs.ui.ac.id>
2017-09-16 11:06:57 +0700

    add task 02

--- week01/what-time-script.sh
+++ week01/what-time-script.sh
@@ -1,4 +1,10 @@
+/bo "Today is $DATE"
+exit 0
 #!/bin/bash
+DATE = `date +%d\ %b\ %Y\ at\ %H:%M:%S`
+echo "Today is $DATE"
+exit 0
+n/bash: DATE: command not found
 DATE = `date +%d\ %b\ %Y\ at\ %H:%M:%S`
 echo "Today is $DATE"
 exit 0
